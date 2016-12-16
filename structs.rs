27950c914ee276315cd44774f44a49529c2651dd
Lukas Andreas Muller <lamuller@protonmail.com>
2016-12-16 13:13:42 -0800

    Add generics

--- structs.rs
+++ structs.rs
@@ -1,13 +1,33 @@
-struct Character {
+struct Human {
     age: i32,
-    height: i32
+    height: i32,
+    school_year: i32
 }
 
 fn main() {
-    let human = Character {
-        age: 16,
-        height: 179
+    let holly = Human {
+        age: 14,
+        height: 165,
+        school_year: 9
     };
 
-    println!("{}", human.age);
+    let array = [
+        Human {
+            age: 14,
+            height: 165,
+            school_year: 9
+        },
+        Human {
+            age: 16,
+            height: 179,
+            school_year: 10
+        }
+    ];
+
+    println!("Holly is {} years old, {} cm tall and currently in grade {}", holly.age, holly.height, holly.school_year);
+
+    for human in &array {
+        println!("{}", human.age);
+    }
+
 }
